89401a6ee6b9c040aa43393084c96be35c668d5a
Nirmal Sharon Joji <sharonjoji99@gmail.com>
2020-08-01 00:04:50 +0530

    welcome

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to sell-that');
 });
 app.listen(PORT, function () {
   console.log('Example app listening on port 3000!');
