c14241007be8a671cb2906973f3503e2e9ed9a05
Nirmal Sharon Joji <sharonjoji99@gmail.com>
2020-08-01 10:47:22 +0530

    chat app

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Welcome to sell-that');
+
+var http = require('http');
+var server = http.Server(app);
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Chat server running');
 });
-app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!');
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.on('message', function(msg) {
+    io.emit('message', msg);
+  });
 });
