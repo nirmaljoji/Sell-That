72f1d0add245fa5fd5f61db64ff29568ba0732ee
Nirmal Sharon Joji <sharonjoji99@gmail.com>
2020-08-02 10:23:08 +0530

    new major change added

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-var PORT = process.env.PORT || 5000;
 var express = require('express');
-var path = require('path');
-var http = require('http');
 var app = express();
-var server = http.Server(app);
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'views')));
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-
-// viewed at http://localhost:8080
+// set the home page route
 app.get('/', function(req, res) {
-    res.sendFile(path.join('/index.html'));
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-server.listen(PORT, function() {
-    console.log('Chat server running');
-  });
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
