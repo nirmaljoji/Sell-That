dbce21d752a5319a1408b3f639300cd202b3fbe3
Rakshana J <47961988+rakshanaj@users.noreply.github.com>
2020-09-25 23:52:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index.ejs');
+	res.render('dashboard.ejs');
 });
 
 app.listen(port, function() {
