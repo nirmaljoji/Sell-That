fa99a6358ad937025ad699b3e6c991139e4e10cb
Rakshana <rakshana.jp@gmail.com>
2020-09-27 05:40:37 +0530

    something

---
+++ public/js/rakshana.js
@@ -0,0 +1,8 @@
+
+    $('#testy').click(function() {
+        alert("post button!!")
+       $('.modal-wrapper').toggleClass('open');
+      $('.page-wrapper').toggleClass('blur');
+       return false;
+    });
+  
