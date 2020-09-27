0ffe64e597f42bee88a93b4bc051215df3e05d6b
Nirmal Sharon Joji <sharonjoji99@gmail.com>
2020-09-27 15:47:24 +0530

    index chnaged

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('dashboard');
+	res.render('index');
 });
 
 app.get('/forumans', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('showans');
+	res.render('');
 });
 
 app.listen(port, function() {
