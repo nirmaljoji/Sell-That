50ac4861ac5ec37f2595dce90327d75cb7cea724
Rakshana <rakshana.jp@gmail.com>
2020-09-27 15:50:59 +0530

    to merge

--- public/js/rakshana.js
+++ public/js/rakshana.js
@@ -42,3 +42,26 @@ function openTab(evt, tab){
 }
 
 //upload button
+
+
+//timer countdown
+
+var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h left";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
